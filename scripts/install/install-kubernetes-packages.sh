f37cde25c8ed7b5d52fc1811f115d28d0e21eb32
svitalii <vitaliy.saliy@gmail.com>
2026-06-29 22:35:26 +0300

    Update Kubernetes package installation script to use batch mode for GPG key import

--- scripts/install/install-kubernetes-packages.sh
+++ scripts/install/install-kubernetes-packages.sh
@@ -15,7 +15,7 @@ apt-get update
 apt-get install -y apt-transport-https ca-certificates curl gpg
 
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL "https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION%.*}/deb/Release.key" | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL "https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION%.*}/deb/Release.key" | gpg --dearmor --batch --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION%.*}/deb/ /" >/etc/apt/sources.list.d/kubernetes.list
